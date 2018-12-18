52f6eed05ff4cc99744094c949fffa4649e91a00
Cong Luwen <congve1@live.com>
2018-12-18 13:57:47 +0800

    dataset

--- image_captioning/data/datasets/coco.py
+++ image_captioning/data/datasets/coco.py
@@ -19,22 +19,24 @@ class COCODataset(torch.utils.data.Dataset):
     ):
         self.root = root
         self.seq_per_img = seq_per_img
-        """
         att_features_file = h5py.File(att_features_file, 'r')
         self.att_features_dataset = att_features_file['att_features']
         fc_features_file = h5py.File(fc_features_file,'r')
         self.fc_features_dataset = fc_features_file['fc_features']
         self.cocoid_dataset = fc_features_file['cocoids']
-        """
 
+        """
         self.att_features_file = att_features_file
         self.fc_features_file = fc_features_file
+        """
+
         with open(encoded_captions_file, 'r') as f:
             self.encoded_captions_file = json.load(f)
         with open(encoded_captions_lens_file, 'r') as f:
             self.encoded_captions_lens_flie = json.load(f)
 
     def __getitem__(self, index):
+        """
         att_features_file = h5py.File(self.att_features_file, 'r')
         att_features_dataset = att_features_file['att_features']
         fc_features_file = h5py.File(self.fc_features_file, 'r')
@@ -47,8 +49,8 @@ class COCODataset(torch.utils.data.Dataset):
             fc_features_dataset[index//self.seq_per_img]
         )
         cocoid = int(cocoid_dataset[index//self.seq_per_img])
-
         """
+
         att_feature = torch.from_numpy(
             self.att_features_dataset[index//self.seq_per_img]
         )
@@ -56,7 +58,6 @@ class COCODataset(torch.utils.data.Dataset):
             self.fc_features_dataset[index//self.seq_per_img]
         )
         cocoid = int(self.cocoid_dataset[index//self.seq_per_img])
-        """
 
         caption = torch.tensor(self.encoded_captions_file[index], dtype=torch.long)
         cap_len = torch.tensor(self.encoded_captions_lens_flie[index], dtype=torch.long)
